1d9700746e258d105f7db670800fbc37cc5d7da1
Rizky <90494968+lordkiki@users.noreply.github.com>
2024-01-23 13:55:01 +0700

    Delete personal directory

--- personal/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Tbcars
-
-class TbcarsSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Tbcars
-        fields = ('id', 'carname', 'carbrand', 'carmodel', 'carprice')
